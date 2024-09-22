49f5b9323da4bf675ddea6bea93a39f30e7b0ce3
Sheikh-Mussadiq <natangkr757@gmail.com>
2024-09-22 23:45:33 +0500

    updated-vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import htmlPurge from 'vite-plugin-purgecss'
+import { defineConfig } from "vite";
+import htmlPurge from "vite-plugin-purgecss";
 
-export default {
-    plugins: [
-        htmlPurge(),
-    ]
-}
+export default defineConfig({
+  base: "/Chefs-Service-Beyond-CSS/", // Replace with your GitHub repository name
+  plugins: [
+    htmlPurge(), // Keep your existing PurgeCSS plugin
+  ],
+});
